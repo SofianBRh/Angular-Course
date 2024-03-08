fd4e83ac190a1ade933e9dd879d07f49f1bac69c
SofianBRh <95184450+SofianBRh@users.noreply.github.com>
2024-03-08 15:08:30 +0100

    DONE

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-angular.module('movieApp', ['pascalprecht.translate']);
+const translationsEn = {
+    "Note:": "Note:",
+    "Rating actuel:": "Current Rating:",
+    "Moyenne du rating:": "Average Rating:",
+    "Nombre de votants:": "Number of Voters:",
+    "Titre:": "Title:"
+  }
+
+  const translationsFr = {
+    "Note:": "Note :",
+    "Rating actuel:": "Note actuelle :",
+    "Moyenne du rating:": "Moyenne des notes :",
+    "Nombre de votants:": "Nombre de votants :",
+    "Titre:": "Titre :"
+  }
+
+
+
+angular.module('movieApp', ['pascalprecht.translate'])
+  .config(['$translateProvider', function ($translateProvider) {
+    $translateProvider.useStaticFilesLoader({
+      prefix: './locale-',
+      suffix: '.json'
+    });
+    
+    $translateProvider.translations('en', translationsEn);
+    $translateProvider.translations('fr', translationsFr);
+    $translateProvider.preferredLanguage('fr');
+  }]);
--- phone-cat.component.js
+++ phone-cat.component.js
@@ -1,65 +1,94 @@
 angular.module('movieApp').component('movieCat', {
   template:
-    '<div ng-repeat="movie in $ctrl.movies" class="my-4 p-4 bg-gray-100 rounded-md">' +
-      '<h2 class="text-xl font-semibold">{{ movie.original_title }}</h2>' +
-      '<img ng-src="https://image.tmdb.org/t/p/w500/{{ movie.poster_path }}" alt="{{ movie.original_title }}" class="my-2 rounded">' +
-      '<p class="text-gray-700">{{ movie.overview }}</p>' +
-      '<div class="flex items-center mt-4">' +
-        '<label class="text-gray-600 mr-2">Note:</label>' +
-        '<select ng-model="movie.rating" ng-options="i for i in [1,2,3,4,5]" ng-change="$ctrl.saveRating(movie)"></select>' +
-        '<span class="ml-2 text-gray-600">Moyenne du rating: {{ movie.averageRating | number:1 }}</span>' +
-        '<span class="ml-2 text-gray-600">Nombre de votants: {{ movie.votesCount }}</span>' +
-      '</div>' +
-    '</div>',
-  controller: function($http) {
+    '<div class="grid grid-cols-2 gap-8">' +
+    '<div class="float-right mt-2">' +
+    '<img src="./englishFlag.png" ng-click="$ctrl.toggleLanguage()" alt="English" ng-show="$ctrl.language === \'fr\'">' +
+    '<img src="./frenchFlag.png" ng-click="$ctrl.toggleLanguage()" alt="French" ng-show="$ctrl.language === \'en\'">' +
+    '</div>' +
+    '<div ng-repeat="movie in $ctrl.movies | orderBy:$ctrl.sortBy" class="my-4 p-4 bg-gray-100 rounded-md">' +
+    '<h2 class="text-xl font-semibold">{{ \'Titre:\' | translate }} {{ movie.translated_title || movie.original_title }}</h2>' +
+    '<img ng-src="https://image.tmdb.org/t/p/w500/{{ movie.poster_path }}" alt="{{ movie.original_title }}" class="my-2 w-1/3 rounded">' +
+    '<p class="text-gray-700">{{ movie.overview }}</p>' +
+    '<div class="flex items-center mt-4">' +
+    '<label class="text-gray-600 mr-2">{{ \'Note:\' | translate }}</label>' +
+    '<select ng-model="movie.rating" ng-options="i for i in [1,2,3,4,5]" ng-change="$ctrl.saveRating(movie)"></select>' +
+    '<span class="ml-2 text-gray-600">{{ \'Rating actuel:\' | translate }} {{ movie.rating }}</span>' +
+    '<span class="ml-2 text-gray-600">{{ \'Moyenne du rating:\' | translate }} {{ movie.averageRating | number:1 }}</span>' +
+    '<span class="ml-2 text-gray-600">{{ \'Nombre de votants:\' | translate }} {{ movie.votesCount }}</span>' +
+    '</div>' +
+    '<div class="mt-2">' +
+    '<h3 class="text-lg font-semibold">{{ \'Commentaires:\' | translate }}</h3>' +
+    '<div ng-repeat="comment in $ctrl.getComments(movie.id) track by $index" class="border p-2 my-2 rounded">' +
+    '<p>{{ comment }}</p>' +
+    '</div>' +
+    '<textarea ng-model="$ctrl.newComment" class="w-full border p-2 my-2 rounded" placeholder="{{ \'Ajouter un commentaire...\' | translate }}"></textarea>' +
+    '<button ng-click="$ctrl.addComment(movie.id)" class="bg-blue-500 text-white p-2 rounded">{{ \'Ajouter\' | translate }}</button>' +
+    '</div>' +
+    '<div class="mt-2">' +
+    '<h3 class="text-lg font-semibold">{{ \'Note Moyenne:\' | translate }} {{ movie.vote_average | number:1 }}</h3>' +
+    '</div>' +
+    '</div>' +
+    '</div>' +
+    '<button ng-click="$ctrl.toggleSort()" class="bg-green-500 text-white p-2 rounded">{{ \'Classer par note moyenne\' | translate }}</button>',
+  controller: function ($http, $translate) {
     const apiKey = '158a78c30db2054d22fa4dc6c45f92f4';
     const url = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=fr-FR&page=1`;
-
+    this.language = 'fr';
     this.movies = [];
+    this.newComment = '';
+    this.sortBy = 'averageRating'; // Initial sorting by averageRating in descending order
+    this.reverseSort = true;
 
-    this.saveRating = function(movie) {
-      // Charge les votes existants depuis le localStorage
+    this.saveRating = function (movie) {
       const existingVotes = JSON.parse(localStorage.getItem(`movie_${movie.id}_votes`)) || [];
-
-      // Ajoute le nouveau vote à la liste
       existingVotes.push(movie.rating);
-
-      // Enregistre la liste des votes et le nombre de votants dans le localStorage
       localStorage.setItem(`movie_${movie.id}_votes`, JSON.stringify(existingVotes));
       localStorage.setItem(`movie_${movie.id}_votesCount`, existingVotes.length);
-
-      // Recalcul de la moyenne du rating pour le film spécifique
       this.calculateAverageRating(movie);
     };
 
-    this.loadRating = function(movieId) {
-      // Charge le dernier vote depuis le localStorage
+    this.loadRating = function (movieId) {
       const existingVotes = JSON.parse(localStorage.getItem(`movie_${movieId}_votes`)) || [];
       return existingVotes.length > 0 ? existingVotes[existingVotes.length - 1] : 0;
     };
 
-    this.calculateAverageRating = function(movie) {
-      // Charge les votes existants depuis le localStorage
+    this.calculateAverageRating = function (movie) {
       const existingVotes = JSON.parse(localStorage.getItem(`movie_${movie.id}_votes`)) || [];
-
-      // Calcule la somme des votes et le nombre de votants
       const totalRating = existingVotes.reduce((acc, curr) => acc + curr, 0);
       const votesCount = existingVotes.length;
-
-      // Calcule la moyenne du rating pour le film spécifique
       movie.averageRating = votesCount > 0 ? totalRating / votesCount : 0;
       movie.votesCount = votesCount;
     };
 
-    // Récupérer une liste de films populaires depuis l'API TMDb
+    this.getComments = function (movieId) {
+      return JSON.parse(localStorage.getItem(`movie_${movieId}_comments`)) || [];
+    };
+
+    this.addComment = function (movieId) {
+      const existingComments = this.getComments(movieId);
+      existingComments.push(this.newComment);
+      localStorage.setItem(`movie_${movieId}_comments`, JSON.stringify(existingComments));
+      this.newComment = '';
+    };
+
+    this.toggleLanguage = function () {
+      this.language = (this.language === 'fr') ? 'en' : 'fr';
+      $translate.use(this.language);
+    };
+
+    this.toggleSort = function () {
+      this.reverseSort = !this.reverseSort;
+      this.sortBy = (this.sortBy === 'vote_average' && this.reverseSort) ? '-vote_average' : 'vote_average';
+    };
+
     $http.get(url).then(response => {
       this.movies = response.data.results.map(movie => ({
         ...movie,
         rating: this.loadRating(movie.id) || 0,
-        averageRating: 0, // Ajout de la propriété averageRating
-        votesCount: 0 // Ajout de la propriété votesCount
+        averageRating: 0,
+        votesCount: 0
       }));
-      console.log(this.movies[0].original_title);
+      console.log(this.movies);
     });
   }
 });
